f64f3800428aa636956f3ae2bd3d617517953da9
NazafarinJ <nazafarin.jalilian@mail.utoronto.ca>
2024-09-16 18:12:50 -0400

    multiset is done

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
     public static void main(String[] args) {
         // TODO complete this psvm main method to replicate the timing experiment from adts.py.
         //      Note: this will be client code of the various other classes needing to be written.
+        //multiset  = [TreeMultiSet(), ArrayListMultiSet(), LinkedListMultiSet()]
+        //for multiset in multisets:
+        //for n in [500, 1000, 2000, 4000]:
+        //profileMultiSet(multiset, n)
+
     }
 }
